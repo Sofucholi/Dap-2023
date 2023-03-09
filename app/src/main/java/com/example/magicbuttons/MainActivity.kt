4f9fde2c67db0c6bb0106645d86fd9236a89a90a
Sofucholi <sofiperezv@gmail.com>
2023-03-09 14:35:10 -0300

    App 2 funcionando

--- app/src/main/java/com/example/magicbuttons/MainActivity.kt
+++ app/src/main/java/com/example/magicbuttons/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.magicbuttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.magicbuttons.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var label :TextView
+    lateinit var btnShow :Button
+    lateinit var btnShow2 :Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        label = findViewById(R.id.textView)
+        btnShow = findViewById(R.id.button)
+        btnShow2 = findViewById(R.id.button2)
+
+        label.text = ""
+
+        btnShow.setOnClickListener{
+            label.text = "Hello World"
+        }
+
+        btnShow2.setOnClickListener{
+            label.text = ""
+        }
     }
 }
