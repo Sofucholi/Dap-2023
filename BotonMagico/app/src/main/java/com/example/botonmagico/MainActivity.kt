eb940d482096b7da99f134f32ca5e84bd5edb672
Sofucholi <sofiperezv@gmail.com>
2023-03-09 13:56:13 -0300

    App funcionando

--- BotonMagico/app/src/main/java/com/example/botonmagico/MainActivity.kt
+++ BotonMagico/app/src/main/java/com/example/botonmagico/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.botonmagico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var label :TextView
+    lateinit var btnShow :Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        label = findViewById(R.id.textViewS)
+        btnShow = findViewById(R.id.button)
+
+        label.text = ""
+
+        btnShow.setOnClickListener{
+            label.text = "Hello World"
+        }
     }
 }
